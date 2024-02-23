ce771436d4907463ba9e469721044c5273b8cc47
Goatbottest <157821463+Goatbottest@users.noreply.github.com>
2024-02-23 23:18:56 +0000

    Puke kalagot Kang replit ka wa Kay angay replit ug github

--- script/callad.js
+++ script/callad.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "callad",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "NTKhang, ManhG Fix Get",
+	credits: "NTKhang, ManhG Fix Get wow",
 	description: "Report bot's error to admin or comment",
 	usages: "[Error encountered or comments]",
 	cooldown: 5,
